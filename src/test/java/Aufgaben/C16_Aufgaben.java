384575d03fbf51c09b39b5a4fca163905c5bca64
Your Name <hesoad@gmail.com>
2023-03-01 21:58:29 +0100

    Aufgabe

--- src/test/java/Aufgaben/C16_Aufgaben.java
+++ src/test/java/Aufgaben/C16_Aufgaben.java
@@ -1,2 +1,29 @@
-package Aufgaben;public class C16_Aufgaben {
+package Aufgaben;
+
+import org.junit.Assert;
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C16_Aufgaben extends TestBase {
+    //1. Launch browser
+    //2. Navigate to url 'http://automationexercise.com'
+    //3. Verify that home page is visible successfully
+
+    @Test
+    public void test01() {
+
+        driver.get("http://automationexercise.com");
+        String expectedUrl = "http://automationexercise.com";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl, actualUrl);
+    }
+    //4. Click on 'Products' button
+    //5. Verify user is navigated to ALL PRODUCTS page successfully
+    //6. Enter product name in search input and click search button
+        @Test
+        public void test02() {
+
+            //7. Verify 'SEARCHED PRODUCTS' is visible
+            //8. Verify all the products related to search are visible
+        }
 }
--- src/test/java/Practice/googleTest.java
+++ src/test/java/Practice/googleTest.java
@@ -1,2 +1,49 @@
-package Practice;public class googleTest {
+package Practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class googleTest {
+    WebDriver driver;
+    // ilgili kurulumlari tamamlayalim
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(14));
+
+    }
+
+
+
+    // Kullanici https://www.google.com adresine gider
+
+    @Test
+    public void googleTest(){
+        driver.get("https://www.google.com");
+
+
+
+
+
+    }
+
+
+    // Kullanici cookies i kabul eder
+    // Arama Kutusuna karsilastirma yapmak istedigi para birimlerini girer
+    // Para birimlerinin karsilastirmasini alir
+    // Karsilastirilacak olan para biriminin 1.5 den kucuk oldugu test edilir
+         @After
+    public void tearDown(){
+        //driver.close();
+
+
+         }
 }
